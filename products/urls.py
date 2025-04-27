dc9ccf38b1d398a2ddd82dae83851e5b78e35d55
syed4.raza <syed4.raza@ril.com>
2025-04-27 19:04:57 +0530

    Lecture 3: APIs created using DRF (GET, POST)

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
     path('welcome/', views.welcome, name='welcome'),
+    path('products/', views.get_products, name='get_products'),  # All Products
+    path('products/<int:id>/', views.get_product, name='get_product'),  # Single Product
+    path('product/', views.create_product, name='create_product'),  # Create Product
+    path('products/available/', views.get_available_products, name='get_available_products'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Products
+from .serializers import ProductSerializer
 
 
 # Create your views here.
@@ -7,3 +11,35 @@ def hello(request):
     return HttpResponse("Hello World")
 def welcome(request):
     return HttpResponse("Welcome to Ecommerce")
+
+# API to get all products
+@api_view(['GET'])
+def get_products(request):
+    products = Products.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+# API to get single product by id
+@api_view(['GET'])
+def get_product(request, id):
+    try:
+        product = Products.objects.get(id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Products.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
+
+# API to create product (POST)
+@api_view(['POST'])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['GET'])
+def get_available_products(request):
+    products = Products.objects.filter(is_available=True)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
